e78e488673c63fdff054cdefc2af25d1e2e32844
Ananda Dwi <anandadwiarifin@gmail.com>
2022-03-23 12:13:40 +0700

    add staging cust

--- models/datavault/sat_customer.sql
+++ models/datavault/sat_customer.sql
@@ -1,32 +1,40 @@
+{% set hash_key = "CUSTOMER_HK" %}
+{% set attributes = ["CUSTOMER_ID"] %}
+
 WITH source_data AS (
-    SELECT a.CUSTOMER_HK, a.HASHDIFF, a.CUSTOMER_NAME, a.CUSTOMER_PHONE, a.CUSTOMER_DOB, a.EFFECTIVE_FROM, a.LOAD_DATE, a.SOURCE
+    SELECT 
+        a.{{hash_key}},
+        a.HASHDIFF,
+        a.CUSTOMER_NAME, a.CUSTOMER_PHONE, a.CUSTOMER_DOB, a.EFFECTIVE_FROM,
+        a.LOAD_DATETIME,
+        a.RECORD_SOURCE
     FROM DBTVAULT.TEST.MY_STAGE AS a
-    WHERE CUSTOMER_HK IS NOT NULL
+    WHERE {{hash_key}} IS NOT NULL
 ),
 
 latest_records AS (
-    SELECT a.CUSTOMER_HK, a.HASHDIFF, a.LOAD_DATE
+    SELECT a.{{hash_key}}, a.HASHDIFF, a.LOAD_DATETIME
     FROM (
-        SELECT c.CUSTOMER_HK, c.HASHDIFF, c.LOAD_DATE, 
+        SELECT c.{{hash_key}}, c.HASHDIFF, c.LOAD_DATETIME, 
         RANK() OVER (
-        PARTITION BY c.CUSTOMER_HK
-        ORDER BY c.LOAD_DATE DESC
+        PARTITION BY c.{{hash_key}}
+        ORDER BY c.LOAD_DATETIME DESC
         ) AS rank
         FROM DBTVAULT.TEST.SATELLITE AS c
         JOIN (  
-        SELECT DISTICT source_data.CUSTOMER_HK
+        SELECT DISTICT source_data.{{hash_key}}
         FROM source_data
         ) AS source_records
-        ON c.CUSTOMER_HK = source_records.CUSTOMER_HK
+        ON c.{{hash_key}} = source_records.{{hash_key}}
         ) AS a
     WHERE a.rank = 1
 ),
 
 records_to_insert AS (
-    SELECT DISTICT e.CUSTOMER_HK, e.HASHDIFF, e.CUSTOMER_NAME, e.CUSTOMER_PHONE, e.CUSTOMER_DOB, e.EFFECTIVE_FROM, e.LOAD_DATE, e.SOURCE
+    SELECT DISTICT e.{{hash_key}}, e.HASHDIFF, e.CUSTOMER_NAME, e.CUSTOMER_PHONE, e.CUSTOMER_DOB, e.EFFECTIVE_FROM, e.LOAD_DATETIME, e.RECORD_SOURCE
     FROM source_data AS e
     LEFT JOIN latest_records
-    ON latest_recods.CUSTOMER_HK = e.CUSTOMER_HK
+    ON latest_recods.{{hash_key}} = e.{{hash_key}}
     WHERE latest_records.HASHDIFF != e.HASHDIFF
     OR latest_records.HASHDIFF IS NULL
 )
