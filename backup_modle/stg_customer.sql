66d7c471bce4431834776251456594d4903f4c56
Ananda Dwi <anandadwiarifin@gmail.com>
2022-03-23 10:38:57 +0700

    update

--- backup_modle/stg_customer.sql
+++ backup_modle/stg_customer.sql
@@ -4,21 +4,25 @@
 source_model: 'raw_customer'
 derived_columns:
   RECORD_SOURCE: "!SAP"
-  LOAD_DATETIME: '2022-03-23T01:51:48.163+0000'
 hashed_columns:
-  CUSTOMER_HK: CUSTOMER_ID
+  CUSTOMER_HK: "CUSTOMER_ID"
   CUSTOMER_HASHDIFF:
     is_hashdiff: true
     columns:
-      - FIRST_NAME
-      - LAST_NAME
-      - EMAIL
+      - "FIRST_NAME"
+      - "LAST_NAME"
+      - "EMAIL"
 {%- endset -%}
 
 {% set metadata_dict = fromyaml(yaml_metadata) %}
 
+SELECT 
+  *,
+  TO_DATE({{ dbt_date.today() }}) AS LOAD_DATE
+FROM (
 {{ dbtvault.stage(include_source_columns=true,
                   source_model=metadata_dict['source_model'],
                   derived_columns=metadata_dict['derived_columns'],
                   hashed_columns=metadata_dict['hashed_columns'],
                   ranked_columns=none) }}
+)
